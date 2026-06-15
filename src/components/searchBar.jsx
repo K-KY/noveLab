bcfa5e91cb19330f008b7c34fea214fc842afee1
K-KY <kyuyoungk@naver.com>
2026-06-15 23:36:01 +0900

    feat: 검색창 컴포넌트

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,32 @@
+import { SearchInput } from "./searchInput.jsx";
+import "../css/searchBar.css";
+
+const SearchBar = () =>  {
+    return (
+        <div className="search-bar">
+            <SearchInput />
+
+            <button className="search-button" type="button" aria-label="검색">
+                <svg
+                    className="search-icon"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M21 21L16.65 16.65M18.5 11C18.5 15.1421 15.1421 18.5 11 18.5C6.85786 18.5 3.5 15.1421
+                        3.5 11C3.5 6.85786 6.85786 3.5 11 3.5C15.1421 3.5 18.5 6.85786 18.5 11Z"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
+            </button>
+        </div>
+    );
+};
+
+export { SearchBar };
---
+++ src/components/searchInput.jsx
@@ -0,0 +1,13 @@
+import "../css/serchIput.css";
+
+const SearchInput = () => {
+    return (
+        <input
+            className="search-input"
+            type="text"
+            placeholder="소설 제목을 입력하세요"
+        />
+    );
+};
+
+export { SearchInput };
