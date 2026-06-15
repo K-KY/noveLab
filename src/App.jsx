226136e313bf884847000c3eaa78a76cc42ee1a1
K-KY <kyuyoungk@naver.com>
2026-06-15 23:38:49 +0900

    feat: 라우터로 홈 화면 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
-import {Header} from "./components/header.jsx";
-function App() {
+import { Header } from "./components/header.jsx";
+import { Home } from "./components/home.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-  return (
-    <>
-      <Header />
-    </>
-  )
+function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
