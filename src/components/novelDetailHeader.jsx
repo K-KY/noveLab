b3e579547488a3a730c07b97622f0ed24f24d2f4
K-KY <kyuyoungk@naver.com>
2026-06-17 23:43:10 +0900

    docs: 소설 상세 페이지 목록으로 돌아가기 버튼 함수 추가

--- src/components/novelDetailHeader.jsx
+++ src/components/novelDetailHeader.jsx
@@ -1,7 +1,13 @@
+import {useNavigate} from "react-router-dom";
+
 const NovelDetailHeader = ({ novel }) => {
+    const navigate = useNavigate()
+    function backPage() {
+        navigate('/')
+    }
     return (
         <section>
-            <button type="button">
+            <button onClick={backPage} type="button">
                 <span>‹</span>
                 목록으로 돌아가기
             </button>
