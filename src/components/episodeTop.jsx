39eedff9b82b25cd8bcca03524d2f6453f43c2ce
K-KY <kyuyoungk@naver.com>
2026-06-19 00:52:35 +0900

    feat: 에피소드 헤더 컴포넌트

---
+++ src/components/episodeTop.jsx
@@ -0,0 +1,24 @@
+import {useNavigate} from "react-router-dom";
+import {EpisodeTitle} from "./episodeTitle.jsx";
+import {TranslateToggleButton} from "./TranslateToggleButton.jsx";
+import "../css/episodeTop.css"
+const EpisodeTop = ({ episode, params }) => {
+
+    const navigate = useNavigate()
+    function backPage() {
+        navigate(`/novels/${params.novelId}`);
+    }
+
+    return (
+        <section className="episode-top">
+            <button className={"ep-back-button"} onClick={backPage} type="button">
+                <span className={"ep-back-icon"}>‹</span>
+                목록으로 돌아가기
+            </button>
+            <EpisodeTitle episode={episode} />
+            <TranslateToggleButton />
+        </section>
+    );
+};
+
+export { EpisodeTop };
