eebbd01d32a69be6c955f6073f0e938ddcc1989c
K-KY <kyuyoungk@naver.com>
2026-06-19 00:45:24 +0900

    feat: 에피소드 내용 컴포넌트

---
+++ src/components/episodeContentBox.jsx
@@ -0,0 +1,15 @@
+import "../css/episodeContentBox.css"
+
+const EpisodeContentBox = ({ paragraphs }) => {
+    return (
+        <article className="episode-content-box">
+            {paragraphs.map((paragraph, index) => (
+                <p className="episode-content-paragraph" key={index}>
+                    {paragraph}
+                </p>
+            ))}
+        </article>
+    );
+};
+
+export { EpisodeContentBox };
