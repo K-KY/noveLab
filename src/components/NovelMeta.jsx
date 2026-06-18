c32412632e2f6ce69fe648cee0f3e7947243397c
K-KY <kyuyoungk@naver.com>
2026-06-18 19:34:55 +0900

    feat: 소설 메타정보 컴포넌트 css

--- src/components/NovelMeta.jsx
+++ src/components/NovelMeta.jsx
@@ -1,22 +1,24 @@
-const NovelMeta = ({ novel }) => {
+import "../css/novelMeta.css"
+
+const NovelMeta = ({novel}) => {
     return (
-        <div>
-            <NovelMetaItem label="평점" value={novel.score} />
-            <NovelMetaItem label="총 회수" value={novel.episodeCount} />
-            <NovelMetaItem label="상태" value={novel.state} highlight />
+        <div className={"novel-meta-list"}>
+            <NovelMetaItem label="평점" value={novel.score}/>
+            <NovelMetaItem label="총 회수" value={novel.episodeCount}/>
+            <NovelMetaItem label="상태" value={novel.state} highlight/>
         </div>
     );
 };
 
-const NovelMetaItem = ({ label, value, highlight = false }) => {
+const NovelMetaItem = ({label, value, highlight = false}) => {
     return (
-        <div>
-            <span>{label}</span>
-            <span>
+        <div className={"novel-meta-item"}>
+            <span className={"novel-meta-label"}>{label}</span>
+            <span className={highlight ? "novel-meta-value highlight" : "novel-meta-value"}>
                 {value}
             </span>
         </div>
     );
 };
 
-export { NovelMeta };
+export {NovelMeta};
