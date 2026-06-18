8f2e6e034b9b23c40b1816ff3260c2c760141814
K-KY <kyuyoungk@naver.com>
2026-06-19 00:46:19 +0900

    feat: 에피소드 다음화, 이전화 이동 버튼 푸터 컴포넌트

---
+++ src/components/episodeNavigation.jsx
@@ -0,0 +1,29 @@
+import "../css/EpisodeNavigation.css";
+
+const EpisodeNavigation = ({ currentEpisode, hasPrev, hasNext }) => {
+    return (
+        <nav className="episode-navigation">
+            <button
+                className="episode-nav-button episode-nav-button-prev"
+                type="button"
+                disabled={!hasPrev}
+            >
+                ← 이전화
+            </button>
+
+            <span className="current-episode-label">
+                제 {currentEpisode}화
+            </span>
+
+            <button
+                className="episode-nav-button episode-nav-button-next"
+                type="button"
+                disabled={!hasNext}
+            >
+                다음화 →
+            </button>
+        </nav>
+    );
+};
+
+export { EpisodeNavigation };
